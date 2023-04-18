51d6964c9b3749c6e783b9c1a7bb4ccc8179afa8
mcastrorib <matheusdcribeiro@gmail.com>
2023-04-18 19:35:13 -0300

    add ilt_with_precond function

--- pyilt.py
+++ pyilt.py
@@ -38,16 +38,23 @@ def ilt(time, decay, reg, nbins=256, tmin=1e-2, tmax=1e4, nprune=512):
 	return t2d, y, err
 
 def ilt_with_factor(time, decay, reg, nbins=256, tmin=1e-2, tmax=1e4, nprune=512, factor=1.0):
-	if type(factor) is not np.ndarray:
-		factor = factor*np.ones(time.size)
+	
 	indexes = logprune_idxs(time.size, nprune)
 	time = time[indexes]
 	decay = decay[indexes]
-	pfactor = factor[indexes]
+	
 	t2d = np.logspace(np.log10(tmin), np.log10(tmax), nbins)
+
 	N = indexes.size
 	G = t2d.size
 	M = np.zeros([N,G])
+
+	if type(factor) is not np.ndarray:
+		print(f"Warning: precond is not an instance of np.ndarray")
+		factor = factor*np.ones(time.size)
+
+	pfactor = factor[indexes]
+
 	for i in range(N):
 		M[i,:] = np.exp(-pfactor[i]*time[i]/t2d)
 	
@@ -60,12 +67,48 @@ def ilt_with_factor(time, decay, reg, nbins=256, tmin=1e-2, tmax=1e4, nprune=512
 
 	return t2d, y, err
 
+def ilt_with_precond(time, decay, reg, nbins=256, tmin=1e-2, tmax=1e4, nprune=512, precond=1.0):
+
+	indexes = logprune_idxs(time.size, nprune)
+	time = time[indexes]
+	decay = decay[indexes]
+
+	t2d = np.logspace(np.log10(tmin), np.log10(tmax), nbins)
+	N = indexes.size
+	G = t2d.size
+	M = np.zeros([N,G])
+
+	if not isinstance(precond, np.ndarray):
+		print(f"Warning: precond is not an instance of np.ndarray")
+		precond = np.ones([N,G])
+
+	if(isinstance(precond, np.ndarray) and not np.array_equal(precond.shape, M.shape)):
+		print(f"Warning: precond [{precond.shape}] and input matrix [{M.shape}] are uncompatible")
+		precond = np.ones([N,G])
+
+	pprecond = precond[indexes, :]
+
+	for i in range(N):
+		for j in range(G):
+			M[i,j] = np.exp(-pprecond[i,j]*time[i]/t2d[j])
+
+	if(reg == 0):
+		y, err = nnls(M,decay)
+	else:
+		Mreg = np.concatenate((M, reg*np.eye(G)))
+		seq = np.concatenate((decay, np.zeros(G)))
+		y, err = nnls(Mreg,seq)
+
+	return t2d, y, err
+
+
 def lcurve(time, decay, nregs=30, reglims=[-1.5,1.5], nbins=256, tmin=1e-2, tmax=1e4, nprune=512):
 	regs = np.logspace(reglims[0], reglims[1], nregs)
 	ts = np.zeros([nregs,nbins])
 	dts = np.zeros([nregs,nbins])
 	errv = np.zeros(nregs)
 	solv = np.zeros(nregs)
+
 	for i, reg in enumerate(regs):
 		t,dt,err=ilt(time,decay,reg,nbins,tmin,tmax,nprune)
 		errv[i] = err
@@ -84,11 +127,40 @@ def lcurve_with_factor(time, decay, nregs=30, reglims=[-1.5,1.5], nbins=256, tmi
 	dts = np.zeros([nregs,nbins])
 	errv = np.zeros(nregs)
 	solv = np.zeros(nregs)
+
 	if type(factor) is not np.ndarray:
+		print(f"Warning: precond is not an instance of np.ndarray")
 		factor = factor*np.ones(time.size)
 
 	for i, reg in enumerate(regs):
 		t,dt,err=ilt_with_factor(time,decay,reg,nbins,tmin,tmax,nprune,factor)
+		errv[i] = err
+		solv[i] = np.sqrt(np.dot(dt,dt))
+		ts[i,:] = t
+		dts[i,:] = dt
+
+		print(f'Iteration {i+1}/{nregs}, Reguralizer {reg:.6f}, Solution {solv[i]:.6f}      ', end='\r')
+	print('')
+
+	return ts, dts, regs, errv, solv
+
+def lcurve_with_precond(time, decay, nregs=30, reglims=[-1.5,1.5], nbins=256, tmin=1e-2, tmax=1e4, nprune=512, precond=1.0):
+	regs = np.logspace(reglims[0], reglims[1], nregs)
+	ts = np.zeros([nregs,nbins])
+	dts = np.zeros([nregs,nbins])
+	errv = np.zeros(nregs)
+	solv = np.zeros(nregs)
+
+	if (type(precond) is not np.ndarray):
+		print(f"Warning: precond is not an instance of np.ndarray")
+		precond = np.ones([nprune, nbins])
+
+	if not np.array_equal(precond.shape, np.array([nprune, nbins])):
+		print(f"Warning: precond [{precond.shape}] and input matrix [{nprune},{nbins}] are uncompatible")
+		precond = np.ones([nprune, nbins])
+
+	for i, reg in enumerate(regs):
+		t,dt,err=ilt_with_precond(time,decay,reg,nbins,tmin,tmax,nprune,precond)
 		errv[i] = err
 		solv[i] = np.sqrt(np.dot(dt,dt))
 		ts[i,:] = t
@@ -123,5 +195,4 @@ def plot_t2dist(bins, amps):
 	# bins, amps, regs, errs, solvs = lcurve(times, decay)
 	# plt.loglog(errs,solvs)
 	# plt.show()
-
 
